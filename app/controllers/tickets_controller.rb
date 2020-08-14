ac243415184eb99779912e9202e28d7ba46aa820
Cody Stair <ccstair27@gmail.com>
2020-08-14 15:25:18 -0400

    Finish adding CRUD to tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,6 +2,7 @@ class TicketsController < ApplicationController
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
   
   def index
+    @tickets = Ticket.all
   end
 
   def show
@@ -9,23 +10,40 @@ class TicketsController < ApplicationController
   end
 
   def new
+    @ticket = Ticket.new
   end
 
   def create
+    @ticket = Ticket.new(ticket_params)
+
+    if @ticket.save
+      redirect_to @ticket
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @ticket.update(ticket_params)
+      flash[:notice] = 'Your ticket was updated.'
+      redirect_to @ticket
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @ticket.destroy
+    redirect_to tickets_path
   end
 
   private
 
   def ticket_params
+    params.require(:ticket).permit(:name, :body, :status, :project_id)
   end
 
   def set_ticket
