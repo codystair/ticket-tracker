c66d51bcda071a1817c7527a2ec252882da03322
Cody Stair <ccstair27@gmail.com>
2020-08-17 17:06:33 -0400

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  
+
+  has_secure_password validations: false
+
+  validates :name, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create, length: {minimum: 5}
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_235446) do
+ActiveRecord::Schema.define(version: 2020_08_17_210003) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2020_08_14_235446) do
     t.string "body"
     t.string "status", default: "New", null: false
     t.integer "project_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
