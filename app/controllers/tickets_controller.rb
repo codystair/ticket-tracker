3ca6de5e99ff2557340a2a1318891831d3b97a0b
Cody Stair <ccstair27@gmail.com>
2020-08-18 14:52:12 -0400

    Add filters to tickets index

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,7 +3,19 @@ class TicketsController < ApplicationController
   before_action :require_session, except: [:show, :index]
   
   def index
-    @tickets = Ticket.all
+    @tickets = if params[:project].present?
+                 Project.find(params[:project]).tickets
+               else
+                 Ticket.all
+               end
+
+    if params[:status].present?
+      @tickets = @tickets.where(status: params[:status])
+    end
+
+    if params[:tag].present?
+      @tickets = @tickets.joins(:tags).where('tags.id': params[:tag])
+    end
   end
 
   def show
