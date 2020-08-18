ec285cf04a18e3933c3022811430380d88bb5205
Cody Stair <ccstair27@gmail.com>
2020-08-18 10:21:07 -0400

    Add Bootstrap to project

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'projects#index'
 
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   resources :projects
   resources :tickets
   resources :tags
