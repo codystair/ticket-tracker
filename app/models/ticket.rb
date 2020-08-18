a9c1a8ac4bd068a47af8643fd2b681caaac60976
Cody Stair <ccstair27@gmail.com>
2020-08-18 12:31:34 -0400

    Add guest restructions and validations to models and views

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,12 @@
 class Ticket < ApplicationRecord
+  STATUSES = %w(new blocked in_progress fixed)
+
   belongs_to :project
 
   has_many :ticket_tags
   has_many :tags, through: :ticket_tags
+
+  validates :name, presence: true
+  validates :status, inclusion: { in: STATUSES }
+  validates :project_id, presence: true
 end
